e95be834d848b025990f6614d5085b4b88149820
Abhii <prabheen09876@gmail.com>
2023-12-13 10:33:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+
+// function multiply(){
+    
+//     a= prompt("enter your no")
+//     b= prompt("enter your no")
+//     // console.log(a*b)
+//     document.write(a*b)
+// }
+// multiply()
+
+
+
+
+
+// function si(){
+//     princi = prompt("enter principle")
+//     rate = prompt("enter rate of intrest")
+//     time = prompt("enter time")
+//     document.write(princi*rate*time/100)
+// }
+// si()
+
+
+
+
+// function fac(n){
+//     let i = 1;
+//     for( i = 1; i<=n; i++){
+//         fac*=i
+//     }
+//     console.log(fac)
+// }
+// n = prompt("enter factorial;")
+// fac(
+// )
+
+
+
+
+//  multiply = function(a,b) {
+//     console.log(a*b)
+//  }
+//  multiply(2,4)
+
+
+
+
+// structuredClone
+
+// var vowel(a) => {
+//     for ( var)
+// }
